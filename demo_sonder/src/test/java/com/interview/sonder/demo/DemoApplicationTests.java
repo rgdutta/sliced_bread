30690d25d1619187d74ca9433c3c1d632674c425
rgdutta <guru.ramanathaiah@sonder.com>
2021-05-08 22:08:35 -0700

    init

---
+++ demo_sonder/src/test/java/com/interview/sonder/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.sonder.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
